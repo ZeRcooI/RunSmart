43163faefce00c86627875f5f18c0d36403d72dc
ZeRcooI <94679102+ZeRcooI@users.noreply.github.com>
2024-03-04 01:23:35 +0300

    Закончил первую попытку с каруселью

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
-  $('.carousel__inner').slick();
+  $('.carousel__inner').slick({
+    speed: 1200,
+    // adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="icons/main/arrow/left.png"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="icons/main/arrow/right.png"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  });
 });
