ad3d0da1ab3bfa9c5f74a0161ccb7e17d2edf57f
ZeRcooI <94679102+ZeRcooI@users.noreply.github.com>
2024-03-08 06:00:22 +0300

    секция каталога готова к адаптации

--- src/js/main.js
+++ src/js/main.js
@@ -32,4 +32,19 @@ $(document).ready(function () {
         .addClass('catalog__content_active');
     },
   );
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content')
+          .eq(i)
+          .toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+  }
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
 });
